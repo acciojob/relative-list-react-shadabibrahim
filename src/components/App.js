e8bb2c2dff2a7fe0607dcc2b0de8cae8ae8322ad
Mohd Shadab <shadab_ibrahim@outlook.com>
2024-06-22 19:38:46 +0530

    submitting code-Sat Jun 22 2024 19:38:45 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const App = () => {
+  const relative = ["Doctor","Atif","Owais","Ehtesham"];
   return (
     <div id="main">
                {/* Do not remove the main div */}
+              <ol key={"relativeList"}>
+                {relative.map((data,index)=>{
+                  return <li key={"relativeListItem"+index+1}>{data}</li>
+                })}
+              </ol>
     </div>
   )
 }
